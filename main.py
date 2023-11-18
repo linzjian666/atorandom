da9a07700095b9c6ecb7dcce401804c9a7bde8fd
linzjian666 <63142068+linzjian666@users.noreply.github.com>
2023-11-18 20:10:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
-import random
-def 生成整数():
-  while True:
-    try:
-      a = int(input("请输入范围前置数："))
-      b = int(input("请输入范围后置数："))
-      if a == b:
-        print("前置数不能等于后置数！")
-      else: 
-        break
-    except:
-      print("请输入正确的整数！")
-      continue 
-  c = random.randint(a,b)
-  print("生成结果:" , c)
+# -*- coding: UTF-8 -*-
+# @Author  : Linzjian666
+# @Time    : 2023/11/18 11:45
+# @Function: Generate random number within a range
 
-def 生成小数():
-  while True:
-    try:
-      a = float(input("请输入范围前置数："))
-      b = float(input("请输入范围后置数："))
-      if a == b:
-        print("前置数不能等于后置数！")
-      else: 
-        break
-    except:
-      print("请输入正确的小数！")
-    continue 
-  c = random.randint(a,b)
-  print("生成结果:" , c)
+import random
+def 获取小数位数(number):
+  decimal_str = str(number).split('.')
+  if len(decimal_str) == 2 and len(decimal_str[1]) != 0:
+      return len(decimal_str[1])
+  else:
+      return 0
+
+def 生成随机数(before,after):
+  try:
+      if float(before) > float(after):
+          print("起始数应小于终止数!")
+      if 获取小数位数(before) == 0 and 获取小数位数(after) == 0:
+          random_num = random.randint(int(before), int(after))
+      else:
+          decimal_places = max(获取小数位数(before), 获取小数位数(after))
+          before = float(before) * 10 ** decimal_places
+          after = float(after) * 10 ** decimal_places
+          random_num = random.randint(int(before), int(after))
+          random_num /= 10 ** decimal_places
+      return random_num
+  except:
+    print("请输入有效的数据!")
 
 if __name__ == "__main__":
-  mode = input("欢迎使用随机数生成器！！\r\n工作模式：\r\n1.整数模式\r\n2.小数模式\r\n请选择(1/2):")
+  print("欢迎使用随机数生成器！！")
   while True:
-    if mode =="1":
-      生成整数()
-    elif mode =="2":
-      生成小数()
-    else :
-      print ("请输入正确的工作模式！")
-      continue
+    before = input("\r\n请输入范围起始数:")
+    after = input("请输入范围终止数:")
+    result = 生成随机数(before,after)
+    print("\r\n生成结果:", result)
